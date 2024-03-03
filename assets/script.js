1289209237b5873d1deb85486e8ffe8b233d9c74
Tania Bezerra <nybrasil@gmail.com>
2024-03-03 18:42:44 -0500

    completed HTML

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,48 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+//});
+$(function () {
+  // Add listener for click events on the save button
+  $(".saveBtn").on("click", function () {
+    // Get the id of the parent time-block
+    var blockId = $(this).closest(".time-block").attr("id");
+    // Get the user input from the textarea within this time-block
+    var userInput = $(this).siblings(".description").val();
+    // Save the user input in local storage using the time-block id as key
+    localStorage.setItem(blockId, userInput);
+  });
+
+  // Function to apply past, present, or future class to each time block
+  function updateTimeBlocks() {
+    // Get the current hour using Day.js
+    var currentHour = dayjs().format("H");
+    // Loop through each time block
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      // Compare the block hour to the current hour and apply classes accordingly
+      if (blockHour < currentHour) {
+        $(this).addClass("past").removeClass("present future");
+      } else if (blockHour == currentHour) {
+        $(this).addClass("present").removeClass("past future");
+      } else {
+        $(this).addClass("future").removeClass("past present");
+      }
+    });
+  }
+
+  // Call updateTimeBlocks initially to set classes on page load
+  updateTimeBlocks();
+
+  // Get user input from local storage and set values of corresponding textareas
+  $(".time-block").each(function () {
+    var blockId = $(this).attr("id");
+    var savedInput = localStorage.getItem(blockId);
+    if (savedInput) {
+      $(this).find(".description").val(savedInput);
+    }
+  });
+
+  // Display the current date in the header of the page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+})
